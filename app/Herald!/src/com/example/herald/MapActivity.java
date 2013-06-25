8bbe67d082108bab80544471ba9c5f0254fc5aa4
jmh9072 <jmh9072@8fc548c9-4552-a627-69db-d7d8598d5f49>
2013-06-25 14:25:46 +0000

    Added test case stubs

--- app/Herald!/src/com/example/herald/MapActivity.java
+++ app/Herald!/src/com/example/herald/MapActivity.java
@@ -3,6 +3,10 @@ package com.example.herald;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @MapActivity - displays the map interface to search for a destination
+ *
+ */
 public class MapActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
