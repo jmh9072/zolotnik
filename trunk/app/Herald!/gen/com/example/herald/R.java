cc2756ff04e303d4166ba2c542f51b1f01fac58e
WolfFish42@gmail.com <WolfFish42@gmail.com@8fc548c9-4552-a627-69db-d7d8598d5f49>
2013-07-18 04:30:02 +0000

    Documentation update, Pause button, release notes

--- trunk/app/Herald!/gen/com/example/herald/R.java
+++ trunk/app/Herald!/gen/com/example/herald/R.java
@@ -27,15 +27,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
+        public static final int action_settings=0x7f090009;
         public static final int contacts_btn=0x7f090001;
         public static final int destination_address=0x7f090002;
         public static final int interval_spinner=0x7f090005;
-        public static final int map=0x7f090007;
+        public static final int map=0x7f090008;
         public static final int map_btn=0x7f090003;
         public static final int recipient_number=0x7f090000;
         public static final int spinner_prompt=0x7f090004;
         public static final int start_button=0x7f090006;
+        public static final int stop_button=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,12 +48,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int address_hint=0x7f050004;
-        public static final int app_map=0x7f050008;
+        public static final int app_map=0x7f050009;
         public static final int app_name=0x7f050000;
         public static final int contacts_string=0x7f050003;
-        public static final int maps_string=0x7f050007;
+        public static final int maps_string=0x7f050008;
         public static final int phone_hint=0x7f050002;
         public static final int start_route=0x7f050006;
+        public static final int stop_route=0x7f050007;
         public static final int update_interval=0x7f050005;
     }
     public static final class style {
