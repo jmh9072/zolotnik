9521571a55fb6acd720d4fecb06b61898a51a75c
WolfFish42@gmail.com <WolfFish42@gmail.com@8fc548c9-4552-a627-69db-d7d8598d5f49>
2013-07-24 23:07:19 +0000

    release notes, fixed ui bug, user stories, updated meeting minutes

--- trunk/app/Herald/gen/com/ucsoftwareeng/herald/R.java
+++ trunk/app/Herald/gen/com/ucsoftwareeng/herald/R.java
@@ -9,7 +9,7 @@ package com.ucsoftwareeng.herald;
 
 public final class R {
     public static final class array {
-        public static int interval_array=0x7f060000;
+        public static final int interval_array=0x7f060000;
     }
     public static final class attr {
     }
@@ -20,50 +20,50 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static int activity_horizontal_margin=0x7f040000;
-        public static int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static int ic_launcher=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static int action_settings=0x7f090009;
-        public static int contacts_btn=0x7f090001;
-        public static int destination_address=0x7f090002;
-        public static int interval_spinner=0x7f090005;
-        public static int map=0x7f090008;
-        public static int map_btn=0x7f090003;
-        public static int recipient_number=0x7f090000;
-        public static int spinner_prompt=0x7f090004;
-        public static int start_button=0x7f090006;
-        public static int stop_button=0x7f090007;
+        public static final int action_settings=0x7f090009;
+        public static final int contacts_btn=0x7f090001;
+        public static final int destination_address=0x7f090002;
+        public static final int interval_spinner=0x7f090005;
+        public static final int map=0x7f090008;
+        public static final int map_btn=0x7f090003;
+        public static final int recipient_number=0x7f090000;
+        public static final int spinner_prompt=0x7f090004;
+        public static final int start_button=0x7f090006;
+        public static final int stop_button=0x7f090007;
     }
     public static final class layout {
-        public static int activity_main=0x7f030000;
-        public static int activity_map=0x7f030001;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_map=0x7f030001;
     }
     public static final class menu {
-        public static int main=0x7f080000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static int action_settings=0x7f050001;
-        public static int address_hint=0x7f050004;
-        public static int apostrophe=0x7f05000d;
-        public static int app_map=0x7f050011;
-        public static int app_name=0x7f050000;
-        public static int arivalMessage=0x7f05000e;
-        public static int contacts_string=0x7f050003;
-        public static int exclamationMark=0x7f05000f;
-        public static int maps_string=0x7f050010;
-        public static int phone_hint=0x7f050002;
-        public static int startRoute1=0x7f050008;
-        public static int startRoute2=0x7f050009;
-        public static int start_route=0x7f050006;
-        public static int stop_route=0x7f050007;
-        public static int travelUpdate1=0x7f05000a;
-        public static int travelUpdate2=0x7f05000b;
-        public static int travelUpdate3=0x7f05000c;
-        public static int update_interval=0x7f050005;
+        public static final int action_settings=0x7f050001;
+        public static final int address_hint=0x7f050004;
+        public static final int apostrophe=0x7f05000d;
+        public static final int app_map=0x7f050011;
+        public static final int app_name=0x7f050000;
+        public static final int arivalMessage=0x7f05000e;
+        public static final int contacts_string=0x7f050003;
+        public static final int exclamationMark=0x7f05000f;
+        public static final int maps_string=0x7f050010;
+        public static final int phone_hint=0x7f050002;
+        public static final int startRoute1=0x7f050008;
+        public static final int startRoute2=0x7f050009;
+        public static final int start_route=0x7f050006;
+        public static final int stop_route=0x7f050007;
+        public static final int travelUpdate1=0x7f05000a;
+        public static final int travelUpdate2=0x7f05000b;
+        public static final int travelUpdate3=0x7f05000c;
+        public static final int update_interval=0x7f050005;
     }
     public static final class style {
         /** 
@@ -87,10 +87,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f070001;
     }
 }
