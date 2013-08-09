4f84db246bd4fe2dc19ff8975a3bb3d249a03905
jmh9072 <jmh9072@8fc548c9-4552-a627-69db-d7d8598d5f49>
2013-08-09 06:14:12 +0000

    Dynamic Current Location working, added signed apk

--- trunk/app/Herald/src/com/ucsoftwareeng/herald/SmsService.java
+++ trunk/app/Herald/src/com/ucsoftwareeng/herald/SmsService.java
@@ -25,14 +25,20 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.maps.GeoPoint;
 
-public class SmsService extends IntentService{
+public class SmsService extends IntentService implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener{
 	
 	private SmsManager sms = SmsManager.getDefault();
 	private Timer timer = new Timer();
@@ -43,7 +49,9 @@ public class SmsService extends IntentService{
 	private String recipient;//holds recipient
 	private int interval; // holds interval
 	private Geocoder coder;
+	private LocationClient locationClient;
 	private LatLng destinationLocation;
+	private long durationTime = 0;
 	
 	private GeoPoint origin;
 	private GeoPoint endpoint;
@@ -57,6 +65,8 @@ public class SmsService extends IntentService{
      */
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		locationClient = new LocationClient(this, this, this);
+		locationClient.connect(); //start keeping location up to date
 		Bundle extras = intent.getExtras();//pulls bundle of variables from mainactivity
 		destination = extras.getString("DESTINATION");//pulls the destination address
 		recipient = extras.getString("RECIPIENT");//pulls the recipient number
@@ -78,15 +88,17 @@ public class SmsService extends IntentService{
 			//Log.v("IOException", "No Latitude/Longitude found for city " + destinationAddress.getText().toString());
 		}
 		
-		
+		while (!locationClient.isConnected()) //wait for locationClient to connect before sending first message
+		{
+			
+		}
 		startRouteMessage();
-		
-		timer.scheduleAtFixedRate(new TimerTask() { 
+		/*timer.scheduleAtFixedRate(new TimerTask() { 
 			@Override 
 			public void run() {
 				travelUpdateMessage(); 
 				} 
-			}, interval, interval);//fires the travel update message after the interval and repeatedly after the interval 
+			}, interval, interval);//fires the travel update message after the interval and repeatedly after the interval*/ 
 	}
     /**
      * @onDestroy - called when the service is stopped
@@ -120,15 +132,25 @@ public class SmsService extends IntentService{
     public void travelUpdateMessage(){
     	getMapData();
     	String message = getString(R.string.travelUpdate1) + eta + getString(R.string.travelUpdate2) + destination; //+ getString(R.string.travelUpdate3) + city + getString(R.string.apostrophe) + state;
+    	
     	int stringLength = message.length();
-    	if(stringLength<=160)
+    	Log.v("SecondsToDestination", String.valueOf(durationTime));
+    	if (durationTime > 120)
     	{
-    		sms.sendTextMessage(recipient, null, message, null , null);
-    	}
+        	if(stringLength<=160)
+        	{
+        		sms.sendTextMessage(recipient, null, message, null , null);
+        	}
+        	else
+        	{
+        		sms.sendMultipartTextMessage(recipient, null, parseString(message), null, null);
+        	}
+        }
     	else
     	{
-    		sms.sendMultipartTextMessage(recipient, null, parseString(message), null, null);
+    		arrivalIndicationMessage();
     	}
+
     }
     /**
      * @arrivalIndicationMessage - builds and sends message sent when user arrives at destination
@@ -145,6 +167,8 @@ public class SmsService extends IntentService{
     	{
     		sms.sendMultipartTextMessage(recipient, null, parseString(message), null, null);
     	}
+    	timer.cancel();
+    	this.stopSelf();
     }
     /**
      * @getMapData - retrieves eta, current city and current state from google maps
@@ -153,12 +177,17 @@ public class SmsService extends IntentService{
     public void getMapData(){
     	//something here to pull data from google maps
 
+    	Location currentLocation = locationClient.getLastLocation();
+    	Log.v("LAT", Double.toString(currentLocation.getLatitude()));
+    	Log.v("LONG", Double.toString(currentLocation.getLongitude()));
+    	
+    	LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
     	try{
-    		eta = getCurrentTravelETA(new LatLng(39.16, -84.45), destinationLocation); //"12 parsecs";//temporary string replaced when data can be retrieved
+    		eta = getCurrentTravelETA(currentLatLng, destinationLocation); //"12 parsecs";//temporary string replaced when data can be retrieved
     	}
     	catch (Exception e)
     	{
-    		
+    		Log.v("getCurrentTravelETA", "error");
     	}
     	//city = "Gotham";//temp string replaced when data can be retrieved
     	//state = "Unified Dakota";//temp string replace when data can be retrieved
@@ -176,9 +205,13 @@ public class SmsService extends IntentService{
     	}
     	return splitMessage;
     }
-    
+    /**
+     * @getCurrentTravelETA - returns time to travel between two Latitude and Longitudes
+     *
+     */
     private String getCurrentTravelETA(LatLng start, LatLng end) throws ClientProtocolException, IOException, JSONException, URISyntaxException
     {
+    //build URL to send to Google to get travel time
     HttpClient httpclient = new DefaultHttpClient();
     StringBuilder urlstring = new StringBuilder();
     urlstring.append("https://maps.googleapis.com/maps/api/directions/json?origin=")
@@ -188,9 +221,9 @@ public class SmsService extends IntentService{
     //urlstring.append("http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=true");
     URI url = new URI(urlstring.toString());
 
-    HttpPost httppost = new HttpPost(url);
+    HttpPost httppost = new HttpPost(url); //send URL
 
-    HttpResponse response = httpclient.execute(httppost);
+    HttpResponse response = httpclient.execute(httppost); //receive response
     HttpEntity entity = response.getEntity();
     InputStream is = null;
     is = entity.getContent();
@@ -205,14 +238,54 @@ public class SmsService extends IntentService{
     is.close();
     reader.close();
     String result = sb.toString();
+    
+    //parse JSON file
     JSONObject jsonObject = new JSONObject(result);
     JSONArray routeArray = jsonObject.getJSONArray("routes");
+    Log.v("route", routeArray.getString(0));
     JSONObject route = routeArray.getJSONObject(0);
-    JSONArray legArray = routeArray.getJSONArray(2);
-    JSONObject leg = legArray.getJSONObject(0);
-    JSONObject distance = leg.getJSONObject("distance");
-    String distanceText = distance.getJSONObject("text").toString();
+    JSONArray legArray = route.getJSONArray("legs");
+    Log.v("legs", legArray.getString(0));
+    JSONObject legs = legArray.getJSONObject(0);
+    JSONObject duration = legs.getJSONObject("duration");
+    Log.v("LOG", duration.getString("text"));
+    String durationText = duration.getString("text");
+    durationTime = duration.getLong("value");
 
-    return distanceText;
+    return durationText;
+    }
+    
+    /**
+     * @onConnected - called when locationClient is connected
+     *
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+    	getMapData();
+    	Log.v("Connected!", "Connected!");
+		timer.scheduleAtFixedRate(new TimerTask() { 
+			@Override 
+			public void run() {
+				travelUpdateMessage();
+				} 
+			}, 60000, interval);//fires the travel update message after interval milliseconds and repeatedly after the interval
+    	
+    }
+    /**
+     * @onDisconnected - called when locationClient is disconnected
+     *
+     */
+    @Override
+    public void onDisconnected() {
+
+    }
+    
+    /**
+     * @onConnectionFailed - called is locationClient connection fails
+     *
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
